afbd28b75972f4a86f11be03783542157ef47d9e
guishijin <420188751@qq.com>
2018-08-10 22:12:06 +0800

    改进Stricky组建，以适应layout中的右上navibar固定显示

--- src/views/layout/Layout.vue
+++ src/views/layout/Layout.vue
@@ -3,62 +3,69 @@
     <div v-if="device==='mobile'&&sidebar.opened" class="drawer-bg" @click="handleClickOutside"></div>
     <sidebar class="sidebar-container"></sidebar>
     <div class="main-container">
-      <navbar></navbar>
-      <tags-view></tags-view>
+      <sticky>
+        <navbar></navbar>
+        <tags-view></tags-view>
+      </sticky>
       <app-main></app-main>
     </div>
   </div>
 </template>
 
 <script>
-import { Navbar, Sidebar, AppMain, TagsView } from './components'
-import ResizeMixin from './mixin/ResizeHandler'
+  import { Navbar, Sidebar, AppMain, TagsView } from './components'
+  import ResizeMixin from './mixin/ResizeHandler'
 
-export default {
-  name: 'layout',
-  components: {
-    Navbar,
-    Sidebar,
-    AppMain,
-    TagsView
-  },
-  mixins: [ResizeMixin],
-  computed: {
-    sidebar() {
-      return this.$store.state.app.sidebar
+  import Sticky from '@/components/Sticky'
+
+  export default {
+    name: 'layout',
+    components: {
+      Navbar,
+      Sidebar,
+      AppMain,
+      TagsView,
+      Sticky
     },
-    device() {
-      return this.$store.state.app.device
+    mixins: [ResizeMixin],
+    computed: {
+      sidebar() {
+        return this.$store.state.app.sidebar
+      },
+      device() {
+        return this.$store.state.app.device
+      },
+      classObj() {
+        return {
+          hideSidebar: !this.sidebar.opened,
+          openSidebar: this.sidebar.opened,
+          withoutAnimation: this.sidebar.withoutAnimation,
+          mobile: this.device === 'mobile'
+        }
+      }
     },
-    classObj() {
-      return {
-        hideSidebar: !this.sidebar.opened,
-        openSidebar: this.sidebar.opened,
-        withoutAnimation: this.sidebar.withoutAnimation,
-        mobile: this.device === 'mobile'
+    methods: {
+      handleClickOutside() {
+        this.$store.dispatch('closeSideBar', { withoutAnimation: false })
       }
     }
-  },
-  methods: {
-    handleClickOutside() {
-      this.$store.dispatch('closeSideBar', { withoutAnimation: false })
-    }
   }
-}
 </script>
 
 <style rel="stylesheet/scss" lang="scss" scoped>
   @import "src/styles/mixin.scss";
+
   .app-wrapper {
     @include clearfix;
     position: relative;
     height: 100%;
     width: 100%;
-    &.mobile.openSidebar{
+    &.mobile.openSidebar {
       position: fixed;
       top: 0;
     }
   }
+
   .drawer-bg {
     background: #000;
     opacity: 0.3;
